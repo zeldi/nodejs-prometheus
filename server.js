83eeb883000a007d736150707a063290bd3c297d
zeldi <zeldi@leafbyte.com>
2020-06-06 19:42:17 +0800

    added initial code

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Register promitheus-client
+const client =  require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+const counter = new client.Counter({
+    name: 'node_request_operation_total',
+    help: 'The total number of processed requests'
+})
+
+
+const histogram = new client.Histogram({
+    name: 'node_request_duration_seconds',
+    help: 'Histogram for the duration in seconds.',
+    bucket: [1,2,5,6,10]
+})
+
+app.get('/', (req, res) => {
+    var start = new Date()
+    var simulateTime = 1000
+
+    setTimeout(function(argument) {
+        var end = new Date() - start
+        histogram.observe(end / 1000); //convert to seconds
+    }, simulateTime)
+
+    counter.inc();
+
+    res.send('Hello World!')
+})
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', client.register.contentType)
+    res.end(client.register.metrics())
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
+/*
+ Two main things the prometheus normally used for:
+ 1. How long a function/operation takes
+ 2. How many time does it occurs
+
+ We gonna use two main prometheus metrics:
+ 1. Prometheus Counter
+ 2. Prometheus Histogram
+ */
